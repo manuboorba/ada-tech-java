ae10da39b445f1d37b971c1e3baf96eacb087ae9
Manuela Cavalcanti <manuelacavalcanti3@gmail.com>
2024-06-08 15:08:46 -0300

    estudando arrays

---
+++ src/main/java/vetores/Arrays.java
@@ -0,0 +1,16 @@
+package vetores;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] numeros = {9, 10, 12, 25, 2};
+        int maior = numeros[0];
+        int menor = numeros[0];
+        int media;
+
+        for (int i=0; i < numeros.length; i++) {
+            if (numeros[i] > maior) {
+
+            }
+        }
+    }
+}
